c7cd63c5d0f77a596bdfd24988dcb0657f113562
Joseph Adogeri <joekadama@hotmail.com>
2024-11-21 13:20:38 -0600

    Updated index.js (server)

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const {dbConnection} = require("./src/configs/mongoDB");
+const {dbConnection} = require("./src/configs/mongoDB");
 require('dotenv').config()
-//const errorHandler = require("./src/middlewares/errorHandler");
+const errorHandler = require("./src/middlewares/errorHandler");
 
 const app = express();
 const port = process.env.PORT | 4000;
 
 
 const cors = require("cors");
-//const {corsOptions} = require("./src/configs/cors")
+const {corsOptions} = require("./src/configs/cors")
 
 
 // Middleware
 
 app.use(express.json());
-//app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(bodyParser.json())
 
 
-// app.use("/api/user", require("./src/routes/userRoutes"));
-// app.use("/api/auth", require("./src/routes/authRoutes"));
+app.use("/api/user", require("./src/routes/userRoutes"));
+ app.use("/api/auth", require("./src/routes/authRoutes"));
 
-//app.use(errorHandler);
-//dbConnection();
+app.use(errorHandler);
+dbConnection();
 
 // Start the server
 app.listen(port, () => {

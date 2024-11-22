f76363a65f3a47c891679776bb54c43f63240de5
jadogeri <jadogeri@uno.edu>
2024-11-21 18:02:02 -0600

    Updated BASE URL IN axios.js (client)

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4000/api";
+const baseURL = process.env.BASE_URL;
 //const baseURL = "http://localhost:4000/api/opao"
 
 const headers ={

77fc753b6093e6439651c72f72060c646be44fc6
Joseph Adogeri <joekadama@hotmail.com>
2024-12-02 14:19:33 -0600

    using 127.0.0.1  in ./client

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const baseURL = process.env.BASE_URL;
 //const baseURL = "http://localhost:4000/api/opao"
-const baseURL = "http://localhost:4000/api"
+const baseURL = "http://127.0.0.1:4000/api"
 
 const headers ={
     'Content-Type': 'application/json',

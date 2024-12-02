d46945e330a6bcba39a17da0091c2d94308618e7
Joseph Adogeri <joekadama@hotmail.com>
2024-12-02 16:03:14 -0600

    fixed bugs in axios.js

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,10 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 //const BASE_URL = "http://localhost:4000/api"
-const BASE_URL = process.env.BASE_URL
-alert(BASE_URL)
+const BASE_URL = process.env.REACT_APP_BASE_URL
+alert(REACT_APP_BASE_URL)
 console.log("BASE URL ===" ,BASE_URL)
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: REACT_APP_BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery,
